f1e6c3a353083045d28ff9fc23b845dff67c5d40
AndysJN <andrewsjn@gmail.com>
2025-10-06 11:35:48 +0200

    Starting a new Window and clearing it.

--- OpenGLTestApp/Main.cpp
+++ OpenGLTestApp/Main.cpp
@@ -1,4 +1,74 @@
+#include <iostream>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+const GLint WIDTH = 800, HEIGHT = 600;
+
 int main()
 {
+	//Initialise GLFW
+	if (!glfwInit())
+	{
+		printf("GLFW Initialisation failed!");
+		glfwTerminate();
+		return 1;
+	}
+
+	// Setup GLFW window properties
+	// OpenGL Version -- 3.3
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+	// Core profile = No Backwards compatibility
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// Allow forward compatibility
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+	GLFWwindow* MainWindow = glfwCreateWindow(WIDTH, HEIGHT, "Test Window", NULL, NULL);
+	
+	if (!MainWindow)
+	{
+		printf("GLFW window creation failed!");
+		glfwTerminate();
+		return 1;
+	}
+
+	// Get Buffer Size Information (Related to viewport and portion used)
+	int BufferWidth, BufferHeight;
+	glfwGetFramebufferSize(MainWindow, &BufferWidth, &BufferHeight);
+
+	// Set context for GLEW to use
+	glfwMakeContextCurrent(MainWindow);
+
+	// Allow modern extension features
+	glewExperimental = GL_TRUE;
+
+	if (glewInit() != GLEW_OK)
+	{
+		printf("Glew Initialisation failed!");
+		glfwDestroyWindow(MainWindow);
+		glfwTerminate();
+		return 1;
+	}
+
+	// Setup viewport size
+	glViewport(0, 0, BufferWidth, BufferHeight);
+
+	// Loop until window closed - click close "x"?
+	while (!glfwWindowShouldClose(MainWindow))
+	{
+		// Get + Hanlde user input events
+		glfwPollEvents();
+
+		// Clear Window
+		glClearColor(1.f, 0.f, 0.f, 1.f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// Draw on the unseen buffer and swap it to be seen. (I think at least, review - Ask GPT?).
+		glfwSwapBuffers(MainWindow);
+	}
+
 	return 0;
 }
