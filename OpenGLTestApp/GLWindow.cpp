cac3bbe8710a444ac2a577e52389aa7ad9060e7b
AndysJN <andrewsjn@gmail.com>
2025-10-10 19:35:46 +0200

    Handle Keyboard and Mouse Input
    Created and Set Callback functions for both, mouse and keyboard

--- OpenGLTestApp/GLWindow.cpp
+++ OpenGLTestApp/GLWindow.cpp
@@ -2,11 +2,18 @@
 
 GLWindow::GLWindow() : GLWindow (800, 600)
 {
+	for (size_t i = 0; i < 1024; ++i)
+	{
+		Keys[i] = false;
+	}
 }
 
 GLWindow::GLWindow(GLint WindowWidth, GLint WindowHeight) : Width(WindowWidth), Height(WindowHeight)
 {
-
+	for (size_t i = 0; i < 1024; ++i)
+	{
+		Keys[i] = false;
+	}
 }
 
 int GLWindow::Initialise()
@@ -45,6 +52,12 @@ int GLWindow::Initialise()
 	// Set context for GLEW to use
 	glfwMakeContextCurrent(MainWindow);
 
+	// Handle Key + Mouse Input
+	CreateCallbacks();
+
+	// Lock Cursor to Screen
+	glfwSetInputMode(MainWindow, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
 	// Allow modern extension features
 	glewExperimental = GL_TRUE;
 
@@ -61,6 +74,73 @@ int GLWindow::Initialise()
 
 	// Setup viewport size
 	glViewport(0, 0, BufferWidth, BufferHeight);
+
+	// Catch the pointer to the window to pass to the callback to select the correct window - Input
+	glfwSetWindowUserPointer(MainWindow, this);
+}
+
+void GLWindow::CreateCallbacks()
+{
+	glfwSetKeyCallback(MainWindow, HandleKeys);
+	glfwSetCursorPosCallback(MainWindow, HandleMouse);
+}
+
+GLfloat GLWindow::GetXChange()
+{
+	GLfloat TheChange = XChange;
+	XChange = 0.0f;
+	return TheChange;
+}
+
+GLfloat GLWindow::GetYChange()
+{
+	GLfloat TheChange = YChange;
+	YChange = 0.0f;
+	return TheChange;
+}
+
+void GLWindow::HandleKeys(GLFWwindow* Window, int Key, int Code, int Action, int Mode)
+{
+	GLWindow* TheWindow = static_cast<GLWindow*>(glfwGetWindowUserPointer(Window));
+
+	if (Key == GLFW_KEY_ESCAPE && Action == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(Window, GL_TRUE);
+	}
+
+	if (Key >= 0 && Key < 1024)
+	{
+		if (Action == GLFW_PRESS)
+		{
+			TheWindow->Keys[Key] = true;
+			printf("Pressed: %d\n", Key);
+		}
+		else if (Action == GLFW_RELEASE)
+		{
+			TheWindow->Keys[Key] = false;
+			printf("Released: %d\n", Key);
+		}
+	}
+}
+
+void GLWindow::HandleMouse(GLFWwindow* Window, double XPos, double YPos)
+{
+	GLWindow* TheWindow = static_cast<GLWindow*>(glfwGetWindowUserPointer(Window));
+
+	if (TheWindow->MouseFirstMoved)
+	{
+		TheWindow->LastX = XPos;
+		TheWindow->LastY = YPos;
+		TheWindow->MouseFirstMoved = false;
+	}
+
+	TheWindow->XChange = XPos - TheWindow->LastX;
+	TheWindow->YChange = TheWindow->LastY - YPos; //To avoid inversion on Mouse Movement. Can improve with a setting to flip.
+
+	TheWindow->LastX = XPos;
+	TheWindow->LastY = YPos;
+
+	printf("x: % .6f, y: %.6f\n", TheWindow->XChange, TheWindow->YChange);
 }
 
 GLWindow::~GLWindow()
--- OpenGLTestApp/GLWindow.h
+++ OpenGLTestApp/GLWindow.h
@@ -13,20 +13,36 @@ public:
 
 	int Initialise();
 
-	~GLWindow();
-
 	inline GLfloat GetBufferWidth() const { return BufferWidth; }
 	inline GLfloat GetBufferHeight() const { return BufferHeight; }
 
 	bool GetShouldClose() { return glfwWindowShouldClose(MainWindow); }
 
+	const bool* GetKeys() const { return Keys; } //Can Improve to avoid passing the entire Array
+
+	GLfloat GetXChange();
+	GLfloat GetYChange();
+
 	void SwapBuffers() { glfwSwapBuffers(MainWindow); }
+
+	~GLWindow();
 
 private:
 	GLFWwindow* MainWindow = nullptr;
 
 	GLint Width, Height;
 	GLint BufferWidth, BufferHeight;
+
+	bool Keys[1024]; //Cubrir los caracteres para tener un booleano por tecla
+	GLfloat LastX;
+	GLfloat LastY;
+	GLfloat XChange;
+	GLfloat YChange;
+	bool MouseFirstMoved;
+
+	void CreateCallbacks();
+	static void HandleKeys(GLFWwindow* Window, int Key, int Code, int Action, int Mode); //Match signature of the callback. (static)
+	static void HandleMouse(GLFWwindow* Window, double XPos, double YPos);
 
 };
 
